b040547318d94543b62f3686234b5f691de8f8f2
hoangdzung <hoangdzung@github.com>
2021-05-19 13:00:52 +0700

    evaluate communication cost

--- eval_com.py
+++ eval_com.py
@@ -3,12 +3,16 @@ import sys
 
 remoteBytesRead = 0
 resultSize = 0
+total = 0
 for line in open(sys.argv[1]):
     if '"Event":"SparkListenerStageCompleted"' not in line:
         continue
     m = re.search('(?<=\"internal\.metrics\.shuffle\.read\.remoteBytesRead\"\,\"Value\"\:)[0-9]+', line)
     if m is not None:
         print("remoteBytesRead", int(m.group(0)))
+        total += int(m.group(0))
     m = re.search('(?<=\"internal\.metrics\.resultSize\"\,\"Value\"\:)[0-9]+', line)
     if m is not None:
         print("resultSize",int(m.group(0)))
+        total += int(m.group(0))
+print(total/10**9)
