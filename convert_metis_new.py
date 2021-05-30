d4df67a76edf04aefae259dfbfe53f31af51ffe0
hoangdzung <hoangdzung@github.com>
2021-05-30 18:27:46 +0700

    partition metis-based force size of core nodes

---
+++ convert_metis_new.py
@@ -0,0 +1,56 @@
+import argparse
+from collections import defaultdict
+from tqdm import tqdm 
+import random 
+import subprocess
+import os 
+import numpy as np 
+import metis 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('whole')
+parser.add_argument('core_size')
+parser.add_argument('npart',type=int)
+parser.add_argument('--feats')
+parser.add_argument('--outdir')
+args = parser.parse_args()
+
+G = nx.read_edgelist(args.whole)
+
+core_nodes = set(random.choice(G))
+while(True):
+    cand_nodes = set()
+    for node in core_nodes:
+        cand_nodes = cand_nodes.union([neigh for neigh in G.neighbor(node) if neigh not in core_nodes]) 
+    if len(cand_nodes) + len(corenodes) < args.core_size:
+        core_nodes = core_nodes.union(cand_nodes)
+    else:
+        cand_nodes = random.sample(list(cand_nodes), k = args.core_size - len(core_nodes))
+        core_nodes = core_nodes.union(cand_nodes)
+        break
+
+not_core_G = G.subgraph([node for node in G if node not in core_node])
+ncut, parts = nxmetis.partition(not_core_G, nparts=args.npart)
+part2nodes = defaultdict(list)
+for node, p in zip(G.nodes, parts):
+    part2nodes[p].append(node) 
+
+if args.outdir is not None:
+    if not os.path.isdir(args.outdir):
+        os.makedirs(args.outdir)
+
+    if args.feats is not None:
+        feats = {}
+        for node, feat in tqdm(enumerate(open(args.feats)), desc="Read feat"):
+            feats[str(node)] = np.array(list(map(float,feat.split())))
+        core_nodes =  list(core_nodes)
+        for p, nodes in part2nodes.items():
+            with open(os.path.join(args.outdir,'part_{}.txt.feat'.format(p)),'w') as f:
+                for node in tqdm(nodes+core_nodes,desc="Write feat {}".format(p)):
+                    f.write("{} {}\n".format(node, " ".join(map(str, feats[node]))))
+    for p, nodes in part2nodes.items():
+        nx.write_edgelist(G.subgraph(nodes+core_nodes), os.path.join(args.outdir,'part_{}.txt'.format(p)), data=False )
+
+print("Ncut: ",ncut),
+print("Avg core deg: ", np.mean(len(G.degree(core_node)) for core_node in core_nodes) )
+print("Part size: ", [len(i) for i in parts] )
